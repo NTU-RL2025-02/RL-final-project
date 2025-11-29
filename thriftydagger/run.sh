87af2f1406b94d733b2de3a85479a1b1fcf81b5a
AaronWu-train <aaronwu0218@gmail.com>
2025-11-30 00:26:31 +0800

    feat: add run script for ThriftyDagger experiment

---
+++ thriftydagger/run.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+# Run ThriftyDagger experiment
+# Usage: bash run.sh 
+
+conda activate rl-final
+
+nohup python3 scripts/run_thriftydagger.py \
+    exp_name \
+    --seed 0 \
+    --device 0 \
+    --iters 20 \
+    --targetrate 0.01 \
+    --expert_policy_file models/model_epoch_2000_low_dim_v15_success_0.5.pth \
+    --recovery_policy_file models/model_epoch_1000.pth \
+    --demonstration_set_file models/model_epoch_2000_low_dim_v15_success_0.5-1000.pkl \
+    --max_expert_query 2000 \
+    --environment NutAssembly \
+
