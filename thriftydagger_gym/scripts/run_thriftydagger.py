0c6d8b3821d03ad57474c96bb9fb85a317dea83e
amber-gpu-ws <amba101065@gmail.com>
2025-12-10 03:06:10 +0800

    fix ep length error

--- thriftydagger_gym/scripts/run_thriftydagger.py
+++ thriftydagger_gym/scripts/run_thriftydagger.py
@@ -162,7 +162,8 @@ def main(args):
     else:
         raise NotImplementedError("This environment is not implemented in this script.")
 
-    max_ep_len = getattr(env, "_max_episode_steps", 1000)
+    
+    max_ep_len = 1000
     gym_cfg = {"MAX_EP_LEN": max_ep_len}
 
     if args.rule_expert == 1:
